5b66b0a9503f7d52f49106edfa899e05c10b4f2e
biklimbs <38551502+biklimbs@users.noreply.github.com>
2019-04-22 00:12:42 +0530

    chatbot app

--- app.py
+++ app.py
@@ -2,25 +2,24 @@ from flask import Flask,render_template,request,make_response,jsonify
 import random
 from total_car_count import *
 import flask
+from recommedation.recommendation_sys import *
+from recommedation.constants_vikas import *
+
+from recommedation import *
 app = Flask(__name__)
 
-#@app.route('/random_num')
-#def test():
-'''
-@app.route('/webhook', methods=['GET', 'POST'])
-def webhook():
-    #rand_num=str(random.randint(0, 10))
-    #print(rand_num)
-    #rand_num_dict={}
-    #rand_num_dict["rand_num"]=rand_num
-    req = request.get_json(force=True)
-    action = req.get('queryResult').get('action')
-    print(action)
-    #{"payload": {"google": {"expectUserResponse": True,"richResponse": {"items": [{"simpleResponse": {"textToSpeech": "this is a simple response"}}]}}}}
-    return flask.make_response(flask.jsonify({"responseId": "ea3d77e8-ae27-41a4-9e1d-174bd461b68c","session": "projects/your-agents-project-id/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0","queryResult": {"queryText": "user's original query to your agent","parameters": {"param": "param value"},"allRequiredParamsPresent": True,"fulfillmentText": "Text defined in Dialogflow's console for the intent that was matched","fulfillmentMessages": [{"text": {"text": ["Text defined in Dialogflow's console for the intent that was matched"]}}],"outputContexts": [{"name": "projects/your-agents-project-id/agent/sessions/88d13aa8-2999-4f71-b233-39cbf3a824a0/contexts/generic","lifespanCount": 5,"parameters": {"param": "param value"}}],"intent": {"name": "projects/your-agents-project-id/agent/intents/29bcd7f8-f717-4261-a8fd-2d3e451b8af8","displayName": "Matched Intent Name"},"intentDetectionConfidence": 1,"diagnosticInfo": {},"languageCode": "en"},"originalDetectIntentRequest": {}}))
+#---Global variable---
+make_g=""
+model_g=""
+year_g=""
+low_mileage_g=0
+high_mileage_g=0
+rating_g=[]
+kuzov_g=""
+mileage_g=""
 
-    #return flask.jsonify(rand_num_dict)
-'''
+# print(make_name)
+
 # function for responses
 def results():
     # build a request object
@@ -41,7 +40,6 @@ def results():
 def total_car_count():
     # build a request object
     req = request.get_json(force=True)
-
     # fetch action from json
     action = req.get('queryResult').get('number')
     print("request from chatbot: "+str(req))
@@ -49,38 +47,163 @@ def total_car_count():
     # return a fulfillment response
     rand_num=str(random.randint(50000,75000))
     print(rand_num)
-    #rand_num_dict={}
-    #rand_num_dict["rand_num"]=rand_num
     return {'fulfillmentText': str(rand_num)}
 
+#---Returns mileage range based on the selected option---
+def select_mileage(input_option):
+    switcher = { 
+        1: "0 and 20000", 
+        2: "20001 and 40000", 
+        3: "40001 and 60000",
+        4: "60001 and 80000", 
+        5: "80001 and 100000", 
+        6: "100001 and 120000", 
+        7: "120001 and 140000",
+        8: "140001 and 160000", 
+        9: ">160000" 
+    }
+    try:
+        return switcher[input_option],True
+    except Exception as e:
+        return str(e),False
 
+#---Maps given number to corresonding rate---
+def map_rating(input_option):
+    switcher = { 
+        'a': "s", 
+        'b': "6",
+        'c': "5", 
+        'd': "4.5", 
+        'e': "4",
+        'f': "3.5",
+        'g': "3",
+        'h': "2",
+        'i': "1",
+        'j': "RA",
+        'k': "R" 
+    }
+    try:
+        return switcher[input_option]
+    except Exception as e:
+        return str(e)
 
 # create a route for webhook
 @app.route('/webhook', methods=['GET', 'POST'])
 def webhook():
-    # return response
-    #res_str=results()
-    if request.headers.get('Content-Type') == 'application/json':
-        req = request.get_json(silent=True)
+    global make_g,model_g,year_g,rating_g,low_mileage_g,high_mileage_g,kuzov_g,mileage_g
+    # make_name= "vikas"
+    req = request.get_json(silent=True)
+    #print(make_name)
+    print(req)
+    intend_name=req.get('queryResult').get('intent').get('displayName')
+    if str(intend_name)=='country':
+        total_vehicle_count=get_total_count()
+        welcome_text="Today we have "+str(total_vehicle_count)+" vehicle in auction. Do you want us to show what we have?"
+        print(welcome_text)
+        res_str={'fulfillmentText': str(welcome_text)}
 
-        # fetch action from json
-        #action = req.get('queryResult').get('number')
-        if req.get("result").get("action") == "@total":
-            rand_num=str(random.randint(50000,75000))
-            res_str={'fulfillmentText': str(rand_num)}
+    elif str(intend_name)=='first_customer_interaction':
+        make_list=str(get_all_make())+"\nAvailable Makes for auction are shown above: "
+        #make_list=get_all_make()
+        res_str={'fulfillmentText': str(make_list)}
+
+    elif str(intend_name)=='make':
+        make_name=req.get('queryResult').get('parameters').get('make')[0]
+        print(make_name)
+        make_g=make_name
+        model_list=str(get_all_model(make_name))+"\nAvailable Model for "+make_g+" are shown above: "
+        res_str={'fulfillmentText': str(model_list)}
+    
+    elif str(intend_name)=='model': 
+        model_name=req.get('queryResult').get('parameters').get('model')[0]
+        model_g=model_name
+        kuzov_list=str(get_all_kuzov(make_g,model_name))+"\nAvailable Kuzov for "+make_g+" and "+model_g+" are shown above: "
+        res_str={'fulfillmentText': str(kuzov_list)}
+    
+    elif str(intend_name)=='kuzov': 
+        kuzov_g=req.get('queryResult').get('parameters').get('kuzov')[0]
+        print(make_g,model_g,year_g)
+        year_list=str(get_all_years(make_g,model_g,kuzov_g))+"\nAvailable Year for auction are shown above:"
+        #year_list=get_all_mileage(make_g,model_g,year_g)
+        res_str={'fulfillmentText': str(year_list)}
+    
+    elif str(intend_name)=='year':
+        year_g=req.get('queryResult').get('parameters').get('year')[0]
+        print(make_g,model_g,year_g,kuzov_g)
+        mileage_list="\n1. Less than 20000 \n2. 20001-40000\n3. 40001-60000 \n4. 60001-80000\
+        \n5. 80001-100000\n6. 100001-120000 \n7. 120001-140000\n8. 140001-160000 \n9. Above 160000\
+        \nPlease select mileage from above" 
+        #mileage_list=get_all_mileage(make_g,model_g,year_g,kuzov_g)
+        res_str={'fulfillmentText': str(mileage_list)}
+    
+    elif str(intend_name)=='mileage': 
+        mileage_option=req.get('queryResult').get('parameters').get('mileage')[0]
+        mileage_option=int(mileage_option)
+        mileage_g,mileage_status=select_mileage(mileage_option)
+        if mileage_status:
+            if make_g!='' and model_g!='':
+                print(make_g,model_g,mileage_g,year_g,kuzov_g)
+                rating_list,rating_status=get_all_rate(make_g,model_g,mileage_g,year_g,kuzov_g)
+                if rating_status:
+                    rating_select_option="\na. <10,000km 12 months \nb. <30,000km 36 months\nc. <50,000km. \nd. <100,000km\
+                    \ne. <150000km\nf. Noticeable large scratches \ng .Many exterior scratches \nh.Bad condition\ni. Flood damaged \nj. Minor Damaged and repair \nk. Damaged and repaired \n Available conditions of cars are given above.\nPlease select from given above: "
+                    low_mileage_g,high_mileage_g=mileage_g.replace("and","-").split("-")
+                    mileage_rc=int((int(str(low_mileage_g).strip())+int(str(high_mileage_g).strip()))/2)
+                    rating=rating_select_option
+                    #rating="Please select rating from given list: "+str(rating_list)
+                else:
+                    rating="No data was found for given specification"
+                    mileage_g=""
+            else:
+                rating="please supply valid make and models"
         else:
-            print("different action")
-        #print("Response to chatbot: "+str(res_str))
-        return make_response(jsonify(res_str))
-    else:
-        print("request not in proper format")
+            rating="please select valid mileage options"
+            mileage_g=""
+        res_str={'fulfillmentText': str(rating)}
+    
+    elif str(intend_name)=='rating': 
+        #print(make_name)
+        rating_g=req.get('queryResult').get('parameters').get('rating')
+        print(rating_g)
+        #print(map_rating(rating_g))
+        rate_rc=map_rating(rating_g[0])
+        rating_g=list(map(map_rating,rating_g))
+        print(rating_g)
+        rating_g = json.dumps(rating_g)
+        #rating_str="you have selected following rating: "+str(rating_g)+" Proceed to get price of the car:"
 
+        if make_g!='' and model_g!='' and str(year_g)!='' and str(kuzov_g)!='':
+            #response_dict=call_bid_lambda("ARU","6","1",make_g,model_g,year_g,"2018","",kuzov_g,[],[],rating_g,[])
+            response_dict=call_bid_lambda("ARU","6","1",make_g,model_g,year_g,"2018",str(low_mileage_g).strip()+"-"+str(high_mileage_g).strip(),kuzov_g,[],[],rating_g,[])
+            print(response_dict)
+            try:
+                d = json.loads(response_dict)
+            except Exception as e:
+                print(str(e))
+            price="The price for "+d["company"]+" "+d["model"]+" having Kuzov "+d["kuzov"]+" is "+str(d["price"])+"\n Do you want recommendations for specified car: "
+            res_str={'fulfillmentText': str(price)}
+        else:
+            mileage_list="please supply valid make and models"
+            res_str={'fulfillmentText': str(mileage_list)}
+        #res_str={'fulfillmentText': str(rating_str)}
+
+
+    elif str(intend_name)=='recommendation_action':
+        if make_g!='' and model_g!='' and str(year_g)!='' and str(kuzov_g)!='':
+            car_details_str,recomm_status=get_recommend_cars(make_g, model_g, year_g,"20000", kuzov_g, "", "", "3.5", "")
+            if recomm_status:
+                res_str={'fulfillmentText': str(car_details_str)}
+            else:
+                res_str={'fulfillmentText': str("No recommendation available")}
+    else:
+        res_str={'fulfillmentText': str("Sorry intend could not be identified")}
+    return make_response(jsonify(res_str))
+    
 
 # create a route for webhook
 @app.route('/total_car_in_auction', methods=['GET', 'POST'])
 def total_car_in_auction():
     # return response
-    
     res_str=total_car_count()
     print("Response to chatbot: "+str(res_str))
     return make_response(jsonify(res_str))
@@ -90,28 +213,14 @@ def get_total_car_count():
     df=get_total_count()
     return "Total car: "+str(df.iloc[0,0])
 
-@app.route('/get_make_model_count')
-def get_make_model_count():
+
+def get_make_model_count(make,model):
     try:
-        if request.method == 'POST':
-            make = request.form['make']
-            model = request.form['model']
-            df=get_make_model_total_count(make,model)
-            return "Total car for "+make+" "+model+": "+str(df.iloc[0,0])
+        df=get_make_model_total_count(make,model)
+        return "Total car for "+make+" "+model+": "+str(df.iloc[0,0])
     except Exception as e:
         print(str(e))
 
-
-'''
-# create a route for webhook
-@app.route('/model_count', methods=['GET', 'POST'])
-def total_car_in_auction():
-    # return response
-    
-    res_str=total_car_count()
-    print("Response to chatbot: "+str(res_str))
-    return make_response(jsonify(res_str))
-'''
 #---function starts here---
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=89)
